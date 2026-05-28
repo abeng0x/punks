9b27c6287b505ec59f37bc092a57a047ee715f1c
earn <earn@abeng0x.local>
2026-05-28 11:58:30 +0700

    update final contract

--- src/erc1155/Punks.sol
+++ src/erc1155/Punks.sol
@@ -8,46 +8,42 @@ contract Punks is ERC1155, Ownable {
 
     uint256 public mintPrice = 20 ether;
 
-    constructor()
-        ERC1155(
-            "https://gateway.pinata.cloud/ipfs/bafybeieut6gw5n4f3dcmucith5iol3jsh5gb7edhmftzn6xr7iqogubb4i/{id}.json"
-        )
-        Ownable(msg.sender)
-    {}
+    mapping(uint256 => string) private tokenURIs;
 
-    function mint(
-        uint256 id
-    )
-        external
-        payable
-    {
+    constructor() ERC1155("") Ownable(msg.sender) {
 
-        require(
-            id >= 1 && id <= 6,
-            "Invalid ID"
-        );
+        tokenURIs[1] =
+        "https://gateway.pinata.cloud/ipfs/bafybeieut6gw5n4f3dcmucith5iol3jsh5gb7edhmftzn6xr7iqogubb4i/1.json";
 
-        require(
-            msg.value >= mintPrice,
-            "Not enough payment"
-        );
+        tokenURIs[2] =
+        "https://gateway.pinata.cloud/ipfs/bafybeieut6gw5n4f3dcmucith5iol3jsh5gb7edhmftzn6xr7iqogubb4i/2.json";
 
-        _mint(
-            msg.sender,
-            id,
-            1,
-            ""
-        );
+        tokenURIs[3] =
+        "https://gateway.pinata.cloud/ipfs/bafybeieut6gw5n4f3dcmucith5iol3jsh5gb7edhmftzn6xr7iqogubb4i/3.json";
+
+        tokenURIs[4] =
+        "https://gateway.pinata.cloud/ipfs/bafybeieut6gw5n4f3dcmucith5iol3jsh5gb7edhmftzn6xr7iqogubb4i/4.json";
+
+        tokenURIs[5] =
+        "https://gateway.pinata.cloud/ipfs/bafybeieut6gw5n4f3dcmucith5iol3jsh5gb7edhmftzn6xr7iqogubb4i/5.json";
+
+        tokenURIs[6] =
+        "https://gateway.pinata.cloud/ipfs/bafybeieut6gw5n4f3dcmucith5iol3jsh5gb7edhmftzn6xr7iqogubb4i/6.json";
 
     }
 
-    function withdraw()
-        external
-        onlyOwner
-    {
-        payable(owner()).transfer(
-            address(this).balance
-        );
+    function uri(uint256 tokenId) public view override returns (string memory) {
+        return tokenURIs[tokenId];
+    }
+
+    function mint(uint256 id) external payable {
+        require(id >= 1 && id <= 6, "Invalid ID");
+        require(msg.value >= mintPrice, "Not enough USDC (native)");
+        _mint(msg.sender, id, 1, "");
+    }
+
+    function withdraw() external onlyOwner {
+        payable(owner()).transfer(address(this).balance);
     }
 
 }
